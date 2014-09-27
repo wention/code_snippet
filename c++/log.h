0469f322571dc55927cd5456e1baa4586a278c57
wention <xuyi.828@gmail.com>
2014-09-27 20:49:36 +0800

    c++ code senipet

---
+++ c++/log.h
@@ -0,0 +1,31 @@
+
+/*
+ * log priorities
+ */
+
+#define	LOG_EMERG	0	/* system is unusable */
+#define	LOG_ALERT	1	/* action must be taken immediately */
+#define	LOG_CRIT	2	/* critical conditions */
+#define	LOG_ERR		3	/* error conditions */
+#define	LOG_WARNING	4	/* warning conditions */
+#define	LOG_NOTICE	5	/* normal but significant condition */
+#define	LOG_INFO	6	/* informational */
+#define	LOG_DEBUG	7	/* debug-level messages */
+
+static const char *syslog_level[] = {
+        "EMERGENCY",
+        "ALERT",
+        "CRITICAL",
+        "ERROR",
+        "WARNING",
+        "NOTICE",
+        "INFO",
+        "DEBUG",
+};
+
+int init_log (log_info * conf);
+
+//   检测pid tid 建立 日志队列
+void log_msg (usigned short log_level, const char *msg, ...);
+
+void shutdown_logging (void);
